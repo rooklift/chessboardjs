bae02d8356dc5005f99f03a441e8627b4de6c72d
rooklift <16438795+rooklift@users.noreply.github.com>
2022-02-01 00:07:05 +0000

    Update board.js

--- board.js
+++ board.js
@@ -1017,12 +1017,20 @@ function castling_rights(board, s) {					// s is the castling string from a FEN
 	// At the moment we support more arbitrary castling rights, maybe that's OK.
 }
 
-function fen_passant_square(board, s) {				// FIXME / TODO
-	if (["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"].includes(s)) {
-		return s;
-	} else {
-		return null;
+function fen_passant_square(board, s) {
+	if (board.active === "w" && ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"].includes(s)) {
+		let col = s.charCodeAt(0) - 97;
+		if (board.get(col, 3) !== "p") return null;					// Check capturable pawn exists.
+		if (col > 0 && board.get(col - 1, 3) === "P") return s;
+		if (col < 7 && board.get(col + 1, 3) === "P") return s;
 	}
+	if (board.active === "b" && ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"].includes(s)) {
+		let col = s.charCodeAt(0) - 97;
+		if (board.get(col, 4) !== "P") return null;					// Check capturable pawn exists.
+		if (col > 0 && board.get(col - 1, 4) === "p") return s;
+		if (col < 7 && board.get(col + 1, 4) === "p") return s;
+	}
+	return null;
 }
 
 function is_normal_chess(board) {
